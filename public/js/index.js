0ef390aa472abd257a81b632d3cc52c8f28f67e4
Souban Khandwani <soubankhandwani11@gmail.com>
2025-05-05 16:37:01 +0530

    feat: Add CORS

--- public/js/index.js
+++ public/js/index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector('#emailForm');
 
 const toast = document.querySelector('.toast');
 
-const baseURL = 'http://localhost:5000';
+const baseURL = 'https://file-sharing-platform-yoy0.onrender.com/';
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const path = require('path');
+const cors = require('cors');
 
 const connectDb = require('./config/db.connection');
 const fileRoutes = require('./routes/file.routes');
@@ -10,13 +11,19 @@ const downloadRoutes = require('./routes/download.routes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(express.json());
-
 connectDb();
 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(','),
+};
+
+app.use(cors(corsOptions));
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.json());
 app.use('/api/files', fileRoutes);
 app.use('/files', showRoutes);
 app.use('/files/download', downloadRoutes);
